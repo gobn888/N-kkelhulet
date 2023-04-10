655b5e349d00dd8d8e74aeb9dfd38fc38d5bbd97
Anna-Btzr <batzeri.anna@gmail.com>
2023-04-10 22:25:31 +0200

    add save and share features

--- src/Kategori24c2.js
+++ src/Kategori24c2.js
@@ -780,15 +780,29 @@ const Kategori24c2 = () => {
         {/* If showResults is true, display the button */}
         {showResults ? (
           <>
-            {/* All your existing code for showResults === true */}
-
             {/* Spacer */}
             <div style={{ padding: "5px" }}></div>
+            {/* Save button */}
+            <button
+              className="btn btn-secondary"
+              style={{ marginLeft: "10px" }}
+            >
+              Lagre produkt
+            </button>
+
+            {/* Share button */}
+            <button
+              className="btn btn-secondary"
+              style={{ marginLeft: "10px" }}
+            >
+              Del produkt
+            </button>
 
             {/* add a new product button */}
             <button
-              className="btn btn-primary"
+              className="btn btn-secondary"
               onClick={() => window.location.reload()}
+              style={{ marginLeft: "10px" }}
             >
               Legg til et nytt produkt
             </button>
@@ -798,15 +812,30 @@ const Kategori24c2 = () => {
         {/* If showResults is false, display the button */}
         {showResults === false && (
           <>
-            {/* All your existing code for showResults === false */}
-
             {/* Spacer */}
             <div style={{ padding: "5px" }}></div>
 
+            {/* Save button */}
+            <button
+              className="btn btn-secondary"
+              style={{ marginLeft: "10px" }}
+            >
+              Lagre produkt
+            </button>
+
+            {/* Share button */}
+            <button
+              className="btn btn-secondary"
+              style={{ marginLeft: "10px" }}
+            >
+              Del produkt
+            </button>
+
             {/* add a new product button */}
             <button
-              className="btn btn-primary"
+              className="btn btn-secondary"
               onClick={() => window.location.reload()}
+              style={{ marginLeft: "10px" }}
             >
               Legg til et nytt produkt
             </button>
